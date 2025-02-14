ffbea6db1a0e1b3da61b768a963145b97c541e85
WeijunChen <1603393839@qq.com>
2025-02-14 11:46:40 +0800

    feat: user module register method(not gateway version)

---
+++ Sparrow-backend/Sparrow-domain/src/main/java/sparrow/garcia/domain/model/entity/user/UserRegisterDTO.java
@@ -0,0 +1,26 @@
+package sparrow.garcia.domain.model.entity.user;
+
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true)
+public class UserRegisterDTO {
+
+    /**
+     * 默认值为@null 索引
+     * */
+    private String email;
+
+    /**
+     * default"小炫雀"
+     * */
+    private String username;
+
+    /**
+     * 加密前的密码
+     * */
+    private String password;
+
+}
--- Sparrow-backend/Sparrow-trigger/src/main/java/sparrow/garcia/trigger/controller/user/UserController.java
+++ Sparrow-backend/Sparrow-trigger/src/main/java/sparrow/garcia/trigger/controller/user/UserController.java
@@ -1,12 +1,16 @@
 package sparrow.garcia.trigger.controller.user;
 
 import jakarta.annotation.Resource;
+import org.sparrow.common.http.ErrorCode;
 import org.sparrow.common.http.Result;
+import org.sparrow.common.http.Results;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import sparrow.garcia.domain.model.entity.user.UserLoginDTO;
+import sparrow.garcia.domain.model.entity.user.UserRegisterDTO;
 import sparrow.garcia.domain.service.user.UserLoginService;
+import sparrow.garcia.domain.service.user.UserRegisterService;
 
 @RestController
 public class UserController {
@@ -14,10 +18,20 @@ public class UserController {
     @Resource
     UserLoginService userLoginService;
 
-    @PostMapping("user/login")
-    public Result userLogin(@RequestBody UserLoginDTO userLoginDTO){
+    @Resource
+    UserRegisterService userRegisterService;
+
+    @PostMapping("/api/user/login")
+    public Result userLogin(@RequestBody UserLoginDTO userLoginDTO) {
         return userLoginService.userLogin(userLoginDTO);
     }
 
+    @PostMapping("api/user/register")
+    public Result userRegister(@RequestBody UserRegisterDTO userRegisterDTO) {
+        if (userRegisterService.createAccount(userRegisterDTO.getEmail()
+                , userRegisterDTO.getUsername()
+                , userRegisterDTO.getPassword())) return Results.success();
+        return Results.failure(ErrorCode.USER_REGISTER_ERROR);
+    }
 
 }
